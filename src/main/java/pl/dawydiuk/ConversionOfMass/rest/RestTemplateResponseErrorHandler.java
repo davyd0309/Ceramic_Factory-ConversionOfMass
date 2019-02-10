cbfb55889b9ea769bf7c20743bb3dbfc1167bbc3
Konrad <konrad.dawydiuk@gmail.com>
2019-02-10 21:13:32 +0100

    add test to MassBuilder

---
+++ src/main/java/pl/dawydiuk/ConversionOfMass/rest/RestTemplateResponseErrorHandler.java
@@ -0,0 +1,26 @@
+package pl.dawydiuk.ConversionOfMass.rest;
+
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
+import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
+
+/**
+ * Created by Judith on 10.02.2019.
+ */
+public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+
+    @Override
+    public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
+        return clientHttpResponse.getStatusCode().series() == CLIENT_ERROR
+                || clientHttpResponse.getStatusCode().series() == SERVER_ERROR;
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
+
+    }
+}
--- src/test/groovy/pl/dawydiuk/ConversionOfMass/builder/MassBuilderTest.groovy
+++ src/test/groovy/pl/dawydiuk/ConversionOfMass/builder/MassBuilderTest.groovy
@@ -1,12 +1,40 @@
 package pl.dawydiuk.ConversionOfMass.builder
 
+import models.Clay
+import models.Kaolinite
+import models.Quartz
+import pl.dawydiuk.ConversionOfMass.rest.RawMeterialsConsumer
+import spock.lang.Specification
 
 /**
  * Created by Judith on 02.02.2019.
  */
-class MassBuilderTest  {
+class MassBuilderTest extends Specification {
 
+    private RawMeterialsConsumer rawMeterialsConsumer = Stub()
+    private MassBuilder builder = new MassBuilder(rawMeterialsConsumer)
 
-    def "CreateMass"() {
+    def "should create mass with success"() {
+        given:
+        rawMeterialsConsumer.getClay() >> Clay.builder().build()
+        rawMeterialsConsumer.getKaolinite() >> Kaolinite.builder().build()
+        rawMeterialsConsumer.getQuartz() >> Quartz.builder().build()
+
+        when:
+        def mass = builder.createMass()
+
+        then:
+        mass != null
+        mass.getId() == 1
+        mass.getWeight() == 100.5d
+    }
+
+    def "should throw exception if raw materials is unavailable"() {
+        given:
+
+        when:
+        def mass = builder.createMass()
+        then:
+        mass == null
     }
 }
